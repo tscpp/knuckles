8844c62a3cce102736438268264f9ee703859587
Elias Skogevall <elias.skogevall@gmail.com>
2024-05-06 18:27:49 +0000

    Fix docs build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,7 +9,7 @@ import footnote from "markdown-it-footnote";
 // https://vitepress.dev/reference/site-config
 const config: UserConfig<ThemeConfig> = {
   title: "Knuckles",
-  base: "/next/",
+  base: "/",
   lastUpdated: true,
 
   // prettier-ignore
