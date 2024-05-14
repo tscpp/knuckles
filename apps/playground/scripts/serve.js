f5e5184b4a5468d4834cdcbe743a4c8aa23fdbe9
Elias Skogevall <elias.skogevall@gmail.com>
2024-05-14 14:58:13 +0000

    Fix formatting

--- apps/playground/scripts/serve.js
+++ apps/playground/scripts/serve.js
@@ -1,6 +1,8 @@
 import { spawn } from "node:child_process";
 
-const subprocess = spawn("pnpm", ["serve", "public", "-l", "3001"], { stdio: "inherit" });
+const subprocess = spawn("pnpm", ["serve", "public", "-l", "3001"], {
+  stdio: "inherit",
+});
 subprocess.on("exit", (code) => {
   throw new Error(`Process exited with code ${code}.`);
 });
