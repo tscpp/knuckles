5f2da3160cf7aca1b7fe5c2fa0adb6951d925ac8
Elias Skogevall <elias.skogevall@gmail.com>
2024-06-10 11:18:18 +0000

    Add "duplicate-binding" std rule

--- packages/analyzer/src/standard/rules.ts
+++ packages/analyzer/src/standard/rules.ts
@@ -1,6 +1,8 @@
+import duplicateBinding from "./rules/duplicate-binding.js";
 import virtualElementEndNotation from "./rules/virtual-element-end-notation.js";
 
 export default [
-  //
+  // alphabetical order
+  duplicateBinding,
   virtualElementEndNotation,
 ];
